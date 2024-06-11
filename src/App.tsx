9cb8db131956cfb06ff620d08b2b761401fabc05
NurbekAskat <Nurbeka738@gmail.com>
2024-06-11 07:52:04 +0600

    Сделано 1 задание, то есть добавлена кнопка для закрывания модального окна в виде крестика

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css';
+import Modal from './components/Modal/Modal';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-
+      <button className="btn btn-primary" onClick={() => setShowModal(true)}>Modal window</button>
+      <Modal show={showModal} title="Modal" onClose={() => setShowModal(false)}>
+        <div className="modal-body">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </div>
+        <div className="modal-footer">
+          <button className="btn btn-danger" onClick={() => setShowModal(false)}>Cancel</button>
+        </div>
+      </Modal>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+interface Props extends React.PropsWithChildren {
+  show: boolean;
+  title: string;
+  onClose: React.MouseEventHandler;
+}
+
+const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
+  const closeButtonStyles = {
+    backgroundImage: 'url("https://img.freepik.com/premium-vector/red-x-symbol_1102-4135.jpg")',
+    backgroundSize: 'contain',
+    width: '25px',
+    height: '25px',
+    cursor: "pointer"
+  };
+
+  return (
+    <>
+      <div
+        className="modal-backdrop show"
+        style={{display: show ? 'block' : 'none'}}
+      />
+      <div className="modal show" style={{display: show ? 'block' : 'none'}} onClick={onClose}>
+        <div className="modal-dialog" onClick={(event) => event.stopPropagation()}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">{title}</h1>
+              <div onClick={onClose} className="border-black ms-auto" style={closeButtonStyles} />
+            </div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
+
